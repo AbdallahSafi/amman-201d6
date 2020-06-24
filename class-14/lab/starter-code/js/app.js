03f1ee0838b7bbc105e766dfdbb512470e2c5ee8
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-06-24 15:29:14 +0300

    catalge functionality done

--- class-14/lab/starter-code/js/app.js
+++ class-14/lab/starter-code/js/app.js
@@ -1,47 +1,35 @@
-'use strict';
-
-var items=document.getElementById('items');
-
-// adding an eventListner to get the product name and product quantitiy in submit
-var formProduct=document.getElementById('catalog');
-formProduct.addEventListener('submit',handelForm)
- 
-function handelForm(event){
-  event.preventDefault();
-  var quantityItems =document.getElementById('quantity')
-  var quantitiyvalue = quantityItems.value;
-  var item_selected = items.options[items.selectedIndex].value;
-// console.log('quantity' , quantitiyvalue)
-// console.log('fdfd',item_selected);
-new CartItem (item_selected,quantitiyvalue);
-
-}
+"use strict";
+var counter = 0;
 // Cart constructor.
-var Cart = function(items) {
+var Cart = function (items) {
   // this.items is an array of CartItem instances.
   this.items = items;
 };
 
-Cart.prototype.addItem = function(product, quantity) {
+Cart.prototype.addItem = function (product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  var productItem = new CartItem(product, quantity);
+  this.items.push(productItem);
 };
 
-Cart.prototype.saveToLocalStorage = function() {
+Cart.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  var cartStr = JSON.stringify(cart);
+  localStorage.setItem('cart', cartStr);
 };
 
-Cart.prototype.removeItem = function(item) {
+Cart.prototype.removeItem = function (item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
 };
 
-var CartItem = function(product, quantity) {
+var CartItem = function (product, quantity) {
   this.product = product;
   this.quantity = quantity;
 };
 
 // Product contructor.
-var Product = function(filePath, name) {
+var Product = function (filePath, name) {
   this.filePath = filePath;
   this.name = name;
   Product.allProducts.push(this);
@@ -49,38 +37,27 @@ var Product = function(filePath, name) {
 Product.allProducts = [];
 
 function generateCatalog() {
-  new Product('assets/bag.jpg', 'Bag');
-  new Product('assets/banana.jpg', 'Banana');
-  new Product('assets/bathroom.jpg', 'Bathroom');
-  new Product('assets/boots.jpg', 'Boots');
-  new Product('assets/breakfast.jpg', 'Breakfast');
-  new Product('assets/bubblegum.jpg', 'Bubblegum');
-  new Product('assets/chair.jpg', 'Chair');
-  new Product('assets/cthulhu.jpg', 'Cthulhu');
-  new Product('assets/dog-duck.jpg', 'Dog-Duck');
-  new Product('assets/dragon.jpg', 'Dragon');
-  new Product('assets/pen.jpg', 'Pen');
-  new Product('assets/pet-sweep.jpg', 'Pet Sweep');
-  new Product('assets/scissors.jpg', 'Scissors');
-  new Product('assets/shark.jpg', 'Shark');
-  new Product('assets/sweep.png', 'Sweep');
-  new Product('assets/tauntaun.jpg', 'Taun-Taun');
-  new Product('assets/unicorn.jpg', 'Unicorn');
-  new Product('assets/usb.gif', 'USB');
-  new Product('assets/water-can.jpg', 'Water Can');
-  new Product('assets/wine-glass.jpg', 'Wine Glass');
+  new Product("assets/bag.jpg", "Bag");
+  new Product("assets/banana.jpg", "Banana");
+  new Product("assets/bathroom.jpg", "Bathroom");
+  new Product("assets/boots.jpg", "Boots");
+  new Product("assets/breakfast.jpg", "Breakfast");
+  new Product("assets/bubblegum.jpg", "Bubblegum");
+  new Product("assets/chair.jpg", "Chair");
+  new Product("assets/cthulhu.jpg", "Cthulhu");
+  new Product("assets/dog-duck.jpg", "Dog-Duck");
+  new Product("assets/dragon.jpg", "Dragon");
+  new Product("assets/pen.jpg", "Pen");
+  new Product("assets/pet-sweep.jpg", "Pet Sweep");
+  new Product("assets/scissors.jpg", "Scissors");
+  new Product("assets/shark.jpg", "Shark");
+  new Product("assets/sweep.png", "Sweep");
+  new Product("assets/tauntaun.jpg", "Taun-Taun");
+  new Product("assets/unicorn.jpg", "Unicorn");
+  new Product("assets/usb.gif", "USB");
+  new Product("assets/water-can.jpg", "Water Can");
+  new Product("assets/wine-glass.jpg", "Wine Glass");
 }
 
 // Initialize the app by creating the big list of products with images and names
 generateCatalog();
-console.log(Product.allProducts);
-selectItems (); //this function will fill the options with the select items
-
-function selectItems (){
-for (var i=0 ;i<Product.allProducts.length ; i++) {
-  var itemsOption=document.createElement('option');
-  itemsOption.setAttribute('value', Product.allProducts[i].name)
-  itemsOption.textContent=Product.allProducts[i].name ;
-  items.appendChild(itemsOption);
-}
-}
--- class-14/lab/starter-code/js/catalog.js
+++ class-14/lab/starter-code/js/catalog.js
@@ -1,6 +1,6 @@
 /* global Product, Cart */
 
-'use strict';
+"use strict";
 
 // Set up an empty cart for use on this page.
 var cart = new Cart([]);
@@ -8,51 +8,73 @@ var cart = new Cart([]);
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
-
   //TODO: Add an <option> tag inside the form's select for each product
-  var selectElement = document.getElementById('items');
+  var selectElement = document.getElementById("items");
   for (var i in Product.allProducts) {
-
+    var itemsOption = document.createElement("option");
+    itemsOption.setAttribute("value", Product.allProducts[i].name);
+    itemsOption.textContent = Product.allProducts[i].name;
+    selectElement.appendChild(itemsOption);
   }
-
 }
 
 // When someone submits the form, we need to add the selected item to the cart
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
-function handleSubmit(event) {
 
+function handleSubmit(event) {
   // TODO: Prevent the page from reloading
+  event.preventDefault();
 
   // Do all the things ...
   addSelectedItemToCart();
   cart.saveToLocalStorage();
   updateCounter();
   updateCartPreview();
-
 }
 
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
+  counter += 1;
   // TODO: suss out the item picked from the select list
+  item_selected = items.options[items.selectedIndex].value;
+
   // TODO: get the quantity
+  quantitiyValue = quantityItems.value;
+
   // TODO: using those, add one item to the Cart
+  cart.addItem(item_selected, quantitiyValue);
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+  // console.log(counter);
+  var couterSpan = document.getElementById("itemCount");
+  couterSpan.textContent = counter;
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
+var cartContentDiv = document.getElementById("cartContents");
+var list = document.createElement("ul");
+cartContentDiv.appendChild(list);
+
 function updateCartPreview() {
   // TODO: Get the item and quantity from the form
   // TODO: Add a new element to the cartContents div with that information
+    var listItem = document.createElement("li");
+    listItem.textContent = cart.items[counter-1].quantity + ' ' + cart.items[counter-1].product;
+    list.appendChild(listItem);
+
 }
 
 // Set up the "submit" event listener on the form.
 // This is the trigger for the app. When a user "submits" the form, it will
 // Call that handleSubmit method above and kick off the whole process
-var catalogForm = document.getElementById('catalog');
-catalogForm.addEventListener('submit', handleSubmit);
+var catalogForm = document.getElementById("catalog");
+var quantityItems = document.getElementById("quantity");
+var item_selected;
+var quantitiyValue;
+catalogForm.addEventListener("submit", handleSubmit);
 
 // Before anything else of value can happen, we need to fill in the select
 // drop down list in the form.
